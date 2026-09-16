959c445024c1715178f15f052743abfaead56b11
Karimakos <jmkarimakos@gmail.com>
2026-09-16 20:00:27 +0300

    starting starter files for a custom frappe app with some patches for lead doctype

---
+++ songa_crm/patches/v1_0/setup_lead_fixed.py
@@ -0,0 +1,60 @@
+
+import frappe
+from frappe.custom.doctype.property_setter.property_setter import (
+    make_property_setter,
+)
+
+
+def execute():
+    configure_lead_status()
+
+
+def configure_lead_status():
+    fields = [
+        {
+            "field": "status",
+            "options": [
+                "Identified",
+                "To Contact",
+                "Contacted",
+                "Engaged",
+                "Onboarded",
+                "Declined",
+            ],
+            "default": "Identified",
+        },
+        {
+            "field": "type",
+            "options": [
+                "",
+                "Driver",
+                "Farmer",
+                "Cooperative",
+                "Dairy Off-taker",
+                "Operating Company Partner",
+                "Stakeholder",
+            ],
+            "default": "",
+        },
+    ]
+
+    for field in fields:
+        make_property_setter(
+            "Lead",
+            field["field"],
+            "options",
+            "\n".join(field["options"]),
+            "Text",
+            validate_fields_for_doctype=False,
+        )
+
+        make_property_setter(
+            "Lead",
+            field["field"],
+            "default",
+            field.get("default"),
+            "data",
+            validate_fields_for_doctype=False,
+        )
+
+        frappe.db.commit()
